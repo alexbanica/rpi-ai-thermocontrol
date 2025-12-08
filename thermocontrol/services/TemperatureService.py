2fb13c4617edae7544fe823afe7a899725d662f1
alexbanica <alex@alexlab.nl>
2025-12-08 13:29:53 +0100

    Update AI module temperature handling and configuration
    - Support multiple hwmon paths for temperature reading with improved error handling.
    - Adjust temperature threshold to 45 in `config.yml`.

--- thermocontrol/services/TemperatureService.py
+++ thermocontrol/services/TemperatureService.py
@@ -10,10 +10,15 @@ class TemperatureService:
 
     def get_temperature_ai_module(self) -> float:
         temp = 0
-        try: 
-            with open(f"/sys/class/hwmon/{self.context.ai_thermo_control_hwmon}/temp1_input", "r") as f:
-                temp = int(f.read().strip())
-            return temp / 1000.0
-        except Exception as e:
-            logging.error("Error occurred during reading AI module temperature: %s", e)
-            return 0
+        hwmon_paths = self.context.ai_thermo_control_hwmon.split(",")
+
+        for hwmon_path in hwmon_paths:
+            try:
+                with open(f"/sys/class/hwmon/{hwmon_path.strip()}/temp1_input", "r") as f:
+                    temp = int(f.read().strip())
+                    return temp / 1000.0
+            except Exception as e:
+                logging.warning("Failed to read temperature from %s: %s", hwmon_path.strip(), e)
+
+        logging.error("Error occurred during reading AI module temperature. All options failed.")
+        return 0
