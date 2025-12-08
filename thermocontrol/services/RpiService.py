e75df25760d8d652a5b4ebd42d313c764f3abbd2
alexbanica <alex@alexlab.nl>
2025-12-08 14:04:56 +0100

    Refactor RpiService cleanup and update AI module temperature threshold
    - Ensure proper shutdown of AI module fan with logging and delay.
    - Adjust temperature threshold to 55 in `config.yml`.

--- thermocontrol/services/RpiService.py
+++ thermocontrol/services/RpiService.py
@@ -2,6 +2,9 @@
 # -*- coding:utf-8 -*-
 
 import logging
+import time
+from time import sleep
+
 from thermocontrol.dto import Context
 from gpiozero import OutputDevice
 
@@ -16,5 +19,9 @@ class RpiService:
 
     def __close__(self) -> None:
         logging.info("Closing RpiService")
-        self.ai_module_fan.off()
-        self.ai_module_fan.close()
+        if self.ai_module_fan.is_active:
+            logging.info("Turning off AI module fan")
+            self.ai_module_fan.off()
+            time.sleep(5)
+        del self.ai_module_fan
+        logging.info("Stopped AI module fan")
